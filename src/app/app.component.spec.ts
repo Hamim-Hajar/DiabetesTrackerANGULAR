fe3d06ab5e00818ed92bbbd253d4f3c07dbd58d7
hajarhjr <hamimhajar85@gmail.com>
2024-06-26 11:56:59 +0100

    commit

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -14,16 +14,16 @@ describe('AppComponent', () => {
     expect(app).toBeTruthy();
   });
 
-  it(`should have the 'Diabete_Tangular' title`, () => {
+  it(`should have the 'Diabete_TAngular' title`, () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.componentInstance;
-    expect(app.title).toEqual('Diabete_Tangular');
+    expect(app.title).toEqual('Diabete_TAngular');
   });
 
   it('should render title', () => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
     const compiled = fixture.nativeElement as HTMLElement;
-    expect(compiled.querySelector('h1')?.textContent).toContain('Hello, Diabete_Tangular');
+    expect(compiled.querySelector('h1')?.textContent).toContain('Hello, Diabete_TAngular');
   });
 });
